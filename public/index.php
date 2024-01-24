41ae539dfd0c74b471fab87c97350242fd7ec1f5
Eddy <eddy.vernet@le-campus-numerique.fr>
2024-01-24 14:57:08 +0100

    Ajout du controller

--- public/index.php
+++ public/index.php
@@ -6,23 +6,25 @@ $action = filter_input(INPUT_GET,'action',FILTER_SANITIZE_SPECIAL_CHARS);
 $action = $action ?? "Accueil";
 
 
-/*switch ($action) {
-    case "Contact" :
+
+
+
+
+
+switch ($action) {
+    /*$case "Contact" :
         require '../ressources/views/                   AREMPLACER.php';
         break;
 
     case "Hobby" :
         require '../ressources/views/                   AREMPLACER.php';
-        break;
+        break;*/
 
     case "Accueil" :
-        require '../ressources/views/                   AREMPLACER.php';
+        require '../app/controllers/homeController.php';
         break;
-
+/*
     default :
-        require '../ressources/views/                   AREMPLACER.php';
+        require '../app/controllers/';*/
 }
-*/
-
-
 
